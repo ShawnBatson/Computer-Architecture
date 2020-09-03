c41a3fa5fa689d22b53b7ae3052e3fd8f4968aa3
Shadewalker1984 <Shadewalker1984@yahoo.com>
2020-09-02 19:43:34 -0500

    working along. Very confusing, and so very much involved

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,12 +2,28 @@
 
 import sys
 
+
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.pc = 0,
+        self.reg = [0] * 8,
+        self.ram = [0] * 256
+        self.running = True
+
+# `PC`: Program Counter, address of the currently executing instruction
+# `IR`: Instruction Register, contains a copy of the currently executing instruction
+# `MAR`: Memory Address Register, holds the memory address we're reading or writing
+# `MDR`: Memory Data Register, holds the value to write or the value just read
+# `FL`: Flags, see below
+
+    def ram_read(self, MAR):
+        self.ram[MAR]
+
+    def ram_write(self, MAR, MDR):
+        self.ram[MAR] = MDR
 
     def load(self):
         """Load a program into memory."""
@@ -18,25 +34,24 @@ class CPU:
 
         program = [
             # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
+            0b10000010,  # LDI R0,8   # THIS is the initial register number 0, load data
+            0b00000000,  # this is the register number to load (0)
+            0b00001000,  # this is the value to load (number 8)
+            0b01000111,  # PRN R0 # this instruction is to print
+            0b00000000,  # this is register 0, the value that above instruction prints
+            0b00000001,  # HLT # this is the halt
         ]
 
         for instruction in program:
             self.ram[address] = instruction
             address += 1
 
-
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        # elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -48,8 +63,8 @@ class CPU:
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            #self.fl,
-            #self.ie,
+            # self.fl,
+            # self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
@@ -60,6 +75,15 @@ class CPU:
 
         print()
 
+# * `IR`: Instruction Register, contains a copy of the currently executing instruction
+
     def run(self):
         """Run the CPU."""
-        pass
+        while self.running:
+            IR = str(self.ram_read(self.pc))
+
+            # HALT BASE CASE
+            if IR == "00000001":
+                # set running to false
+                self.running = False
+                self.pc += 1
